a7530eab28c1634db2f6523d33feab9762b4281b
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-11-19 10:34:35 +0100

    Ajout d'éléments du jeu (disques, rectangle, étoile, texte)

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ const config = {
     width: 800,
     mode: Phaser.Scale.ScaleModes.FIT,
   },
-  scene: {},
+  scene: {
+    create() {
+      this.add.circle(200, 200, 25, 0xff0000);
+      this.add.circle(600, 200, 25, 0x0000ff);
+      this.add.text(400, 0, "A first scene").setOrigin(0.5, 0);
+      this.add.star(400, 300, 5, 32, 64, 0xffff00).setOrigin(0.5, 0.5);
+      this.add.rectangle(400, 550, 50, 15, 0x00ff00).setOrigin(0.5, 0.5);
+    },
+  },
 };
 
 // eslint-disable-next-line no-new -- controle désactiver
