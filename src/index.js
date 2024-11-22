46ce3f236fc7e1b5109b5ecba5264ef07d95945f
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-11-22 11:52:34 +0100

    Gestion des touches dans la classe Player

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Phaser from "phaser";
 import config from "./config.js";
+import { Player } from "./object/Player.js";
 
 const firstScene = {
   create() {
     const centreLargeur = this.scale.width / 2;
     const centreHauteur = this.scale.height / 2;
+
     this.add.circle(200, 200, 25, 0xff0000);
     this.add.circle(600, 200, 25, 0x0000ff);
     this.add.text(centreLargeur, 0, "A first scene").setOrigin(0.5, 0);
@@ -12,22 +14,11 @@ const firstScene = {
       .star(centreLargeur, centreHauteur, 5, 32, 64, 0xffff00)
       .setOrigin(0.5, 0.5);
 
-    const rectangle = this.add
-      .rectangle(centreLargeur, 550, 50, 15, 0x00ff00)
-      .setOrigin(0.5, 0.5);
-
-    const keys = this.input.keyboard.addKeys({
+    const player = new Player(this);
+    player.handleKeys({
       left: Phaser.Input.Keyboard.KeyCodes.LEFT,
       right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
-      space: Phaser.Input.Keyboard.KeyCodes.SPACE,
     });
-
-    keys.left.on("down", () => rectangle.body.setVelocityX(-500));
-    keys.right.on("down", () => rectangle.body.setVelocityX(500));
-    keys.left.on("up", () => rectangle.body.setVelocityX(0));
-    keys.right.on("up", () => rectangle.body.setVelocityX(0));
-    keys.space.on("down", () => this.cameras.main.flash(500, 255, 255, 255));
-    this.physics.add.existing(rectangle);
   },
 };
 
---
+++ src/object/Player.js
@@ -0,0 +1,38 @@
+import Phaser from "phaser";
+
+/**
+ *
+ */
+export class Player extends Phaser.GameObjects.Rectangle {
+  constructor(scene, width = 50, height = 20, color = 0xffffff) {
+    super(
+      scene,
+      scene.game.canvas.width / 2,
+      scene.game.canvas.height - 50,
+      width,
+      height,
+      color,
+    );
+
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+  }
+
+  handleKeys(keys) {
+    this.keys = this.scene.input.keyboard.addKeys(keys);
+    this.scene.input.keyboard.on("keydown", this.#handleMove, this);
+    this.scene.input.keyboard.on("keyup", this.#handleMove, this);
+  }
+
+  #handleMove() {
+    const leftDown = this.keys.left.isDown;
+    const rightDown = this.keys.right.isDown;
+    this.body.setVelocityX(0);
+
+    if (leftDown && !rightDown) {
+      this.body.setVelocityX(-500);
+    } else if (rightDown && !leftDown) {
+      this.body.setVelocityX(500);
+    }
+  }
+}
