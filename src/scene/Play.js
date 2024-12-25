a94c9b9824640548deb3b3003364b8fd75dc4fe6
corentinmrcx <corentinmarcoux51@gmail.com>
2024-12-25 13:42:46 +0100

    Intégration des classes Laser et GroupLaser pour les affichers sur l'ecran de jeu

--- src/scene/Play.js
+++ src/scene/Play.js
@@ -15,10 +15,16 @@ export class Play extends Phaser.Scene {
     const centreHauteur = this.scale.height / 2;
     this.add.text(centreLargeur, 0, "A first scene").setOrigin(0.5, 0);
 
-    const player = new Player(this);
-    player.handleKeys({
+    this.player = new Player(this);
+    this.laser = new GroupLaser(this, 5);
+
+    const keys = {
       left: Phaser.Input.Keyboard.KeyCodes.LEFT,
       right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
-    });
+      space: Phaser.Input.Keyboard.KeyCodes.SPACE,
+    };
+
+    this.player.handleKeys(keys);
+    this.laser.handleKeysFire(keys);
   }
 }
