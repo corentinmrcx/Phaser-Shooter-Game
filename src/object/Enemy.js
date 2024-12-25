29e9cce6aa2e12b30c086f52f116fa00bc94bc10
corentinmrcx <corentinmarcoux51@gmail.com>
2024-12-25 18:37:32 +0100

    Ajout des méthodes preUpdate() et getRadius()

--- src/object/Enemy.js
+++ src/object/Enemy.js
@@ -15,4 +15,15 @@ export class Enemy extends Phaser.GameObjects.Arc {
     scene.add.existing(this);
     scene.physics.add.existing(this);
   }
+
+  preUpdate() {
+    if (this.y > this.scene.game.canvas.height) {
+      this.setActive(false);
+      this.setVisible(false);
+    }
+  }
+
+  getRadius() {
+    return this.radius;
+  }
 }
