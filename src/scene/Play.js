a96e4dc1f5fb318ac4468f7fcb08b5f87f26a1f3
corentinmrcx <corentinmarcoux51@gmail.com>
2024-12-25 13:34:00 +0100

    Suppression des objets inutiles crée précédement

--- src/scene/Play.js
+++ src/scene/Play.js
@@ -1,5 +1,7 @@
 import Phaser from "phaser";
 import { Player } from "../object/Player.js";
+import { GroupLaser } from "../object/GroupLaser.js";
+import { Laser } from "../object/Laser.js";
 
 /**
  *
@@ -11,13 +13,7 @@ export class Play extends Phaser.Scene {
   create() {
     const centreLargeur = this.scale.width / 2;
     const centreHauteur = this.scale.height / 2;
-
-    this.add.circle(200, 200, 25, 0xff0000);
-    this.add.circle(600, 200, 25, 0x0000ff);
     this.add.text(centreLargeur, 0, "A first scene").setOrigin(0.5, 0);
-    this.add
-      .star(centreLargeur, centreHauteur, 5, 32, 64, 0xffff00)
-      .setOrigin(0.5, 0.5);
 
     const player = new Player(this);
     player.handleKeys({
